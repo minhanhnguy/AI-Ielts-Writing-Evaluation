9c9b2a8e3d2da05ef772515bf66c67b9e0a24016
minhanhnguy <nguy0335@vandals.uidaho.edu>
2024-09-12 03:28:52 -0700

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,35 @@
+"use client";
 import { Task2Loader } from "@/components/task2";
+import { Task1Loader } from "@/components/task1";
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
 
 import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
+    const [task, setTask] = useState(true);
+
+    function changeTask() {
+        setTask(!task); 
+    }
+
     return (
         <div className="text-center">
+            <Tabs
+                defaultValue="task-2"
+                className="w-[100px] fixed top-20 left-9"
+            >
+                <TabsList>
+                    <TabsTrigger value="task-1" onClick={changeTask}>Task 1</TabsTrigger>
+                    <TabsTrigger value="task-2" onClick={changeTask}>Task 2</TabsTrigger>
+                </TabsList>
+            </Tabs>
             <h1 className="text-3xl mt-2 font-semibold">
                 Ielts AI Writing Evaluation
+                </h1>
                 <ModeToggle />
-            </h1>
-            <Task2Loader></Task2Loader>
+                {task ? <Task2Loader /> : <Task1Loader />}
+            
         </div>
     );
 }
--- src/components/task1.tsx
+++ src/components/task1.tsx
@@ -1,15 +1,19 @@
 "use client";
 import { useState, useEffect } from "react";
 
-import { useWindowSize } from "rooks";
 import { Textarea } from "@/components/ui/textarea";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
-import { Instruction } from "@/components/systemInstructorTask2";
+import * as fs from "fs";
+import { useWindowSize } from "rooks";
+import { Instruction } from "@/components/systemInstructorTask1";
 import { GoogleGenerativeAI } from "@google/generative-AI";
 import { API_KEY } from "@/app/config";
 import { Progress } from "@/components/ui/progress";
+import _default from "next/dist/client/router";
 
 function formatResponse({
     output = null,
@@ -59,13 +63,19 @@ export function ProgressBar() {
     return <Progress value={progress} className="w-[60%] m-auto mt-10" />;
 }
 
-export function Task2Loader() {
+export function Task1Loader() {
     //set up Input Card
     const { innerWidth, innerHeight, outerHeight, outerWidth } =
         useWindowSize();
     if (!innerWidth || !innerHeight) return;
+    
     const cardWidth = innerWidth / 2 - 20;
     const cardHeight = innerHeight - 80;
+
+    const [fileData, setFileData] = useState<{
+        data: string;
+        mimeType: string;
+    } | null>(null);
 
     //set up Gemini
     const apiKey = API_KEY;
@@ -95,6 +105,25 @@ export function Task2Loader() {
     //set up Loading
     const [loading, setLoading] = useState(false);
 
+    function fileToGenerativePart(file: File) {
+        const reader = new FileReader();
+
+        // Read the file as a Data URL (base64 encoded)
+        reader.onloadend = () => {
+            const base64Data = reader.result as string; 
+            setFileData({
+                data: base64Data.split(",")[1], 
+                mimeType: file.type,
+            });
+            console.log(fileData);
+        };
+        if (file instanceof File) {
+            reader.readAsDataURL(file); // Correct usage
+        } else {
+            console.error("Provided input is not a File object");
+        }
+    }
+
     async function handleSubmit(
         e: React.FormEvent<HTMLFormElement>
     ): Promise<void> {
@@ -104,13 +133,22 @@ export function Task2Loader() {
         const formJson: Record<string, any> = Object.fromEntries(
             new FormData(form).entries()
         );
-
-        const result = await chatSession.sendMessage(
+        if (!formJson.Topic) return;
+        console.log(formJson.Topic);
+        fileToGenerativePart(formJson.Topic);
+        if(!fileData) return;
+        const result = await model.generateContent([formJson.Essay, {inlineData: fileData}]);
+        const responseText = result.response.text();
+        console.log(responseText);
+        setResponse(formatResponse({output: responseText, loading: false}));
+        setLoading(false);
+        /*const result = await chatSession.sendMessage(
             "Topic: " + formJson.Topic + "\n Essay: " + formJson.Essay
         );
         const responseText = await result.response.text();
         setResponse(formatResponse({ output: responseText, loading: false }));
         setLoading(false); // Set loading to false when response is received
+        */
     }
 
     return (
@@ -120,18 +158,19 @@ export function Task2Loader() {
                 className="mt-4"
             >
                 <CardHeader className="items-center">
-                    <CardTitle className="text-2xl">Task 2</CardTitle>
+                    <CardTitle className="text-2xl">Task 1</CardTitle>
                 </CardHeader>
                 <CardContent className="flex flex-col">
                     <form method="post" onSubmit={handleSubmit}>
                         <div className="my-4 text-xl mt-[-10px]">
                             Topic Question:
                         </div>
-                        <Textarea
-                            className="resize-none font-normal text-base h-40 overflow-y-auto focus:!ring-transparent"
-                            placeholder="Enter Your Topic Question..."
-                            name="Topic"
-                        />
+                        <div className="grid w-full max-w-sm items-center gap-1.5">
+                            <Label htmlFor="picture" className="mb-[5px]">
+                                Picture (.png, .jpg, jpeg, etc)
+                            </Label>
+                            <Input id="picture" type="file" name="Topic" />
+                        </div>
                         <div className="my-4 text-xl">Essay Input:</div>
                         <Textarea
                             className="resize-none font-normal text-base h-96 overflow-y-auto focus:!ring-transparent"
@@ -140,8 +179,7 @@ export function Task2Loader() {
                         />
                         <Button
                             type="submit"
-                            className="fixed bottom-10"
-                            style={{left: cardWidth - 88}} 
+                            className="fixed bottom-10 left-button"
                         >
                             Submit
                         </Button>
